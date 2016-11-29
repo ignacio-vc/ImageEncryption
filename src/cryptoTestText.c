5c6480211b0545845e599af84ea65a28d47ddb25
jatib <jatib@ciencias.unam.mx>
2016-11-29 09:04:27 -0600

    programa de prueba de algoritmo Cesar para encriptación y el algoritmo Afin escrito en C

---
+++ src/cryptoTestText.c
@@ -0,0 +1,5 @@
+/*Chicos, estoy probando unos algoritmos para familiarizarme con esto, 
+y cómo lo que hablo más fluido es C...pos :P prometo traducir a py...
+Nacho, resulta que 
+*/
+#include <stdio.h>
