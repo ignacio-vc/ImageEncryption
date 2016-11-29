797655212d92f60e219c2bbe860af4c53db6f146
adriross <adriross93@gmail.com>
2016-11-28 19:07:23 -0600

    correccion errores

--- src/merge-example.py
+++ src/merge-example.py
@@ -1,6 +1,6 @@
 #Hola, esta es una prueba para que vean como son los merges
-#ESTA ES LA LINEA 1.5
+#ESTA ES LA LINEA 1.49
 #Esta es la segunda linea.
 #Esta es la tercera linea.
-#LINEA 3.5
+#ESTA ES LA LINEA 3.5
 #Esta es la cuarta linea.
